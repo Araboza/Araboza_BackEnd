96b446802a1e8ffb170b238f28d2ed39e1fb900c
김현승 <80810303+khs3994@users.noreply.github.com>
2021-12-30 15:52:25 +0900

    add swagger
    swagger 추가

--- src/dto/user.dto.ts
+++ src/dto/user.dto.ts
@@ -1,15 +1,36 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsOptional, IsString, IsUrl } from 'class-validator';
 
 export class UserDto {
+  @ApiProperty({
+    title:'사용자 이름',
+    description: '사용자의 이름입니다',
+    example: '변찬우',
+  })
   @IsString()
   name: string;
 
+  @ApiProperty({
+    title:'사용자 이메일',
+    description: '사용자의 이메일 입니다',
+    example: 'chanwoo@teemolove.com',
+  })
   @IsString()
   email: string;
 
+  @ApiProperty({
+    title:'사용자 고유 키',
+    description: '사용자가 구글 로그인 할시 받는 고유키 입니다',
+    example: '9reriqr1324234',
+  })
   @IsString()
   sub: string;
 
+  @ApiProperty({
+    title:'사용자 프로필',
+    description: '사용자의 프로필 사진 URL 입니다',
+    example: 'http://chanwoo123.Teemosarang',
+  })
   @IsUrl()
   picture: string;
 }
@@ -35,23 +56,49 @@ export class User2 {
 }
 
 export class registerDto {
+  @ApiProperty({
+    title:'회원가입시 사용자 토큰',
+    description: '회원가입시 나오는 사용자의 토큰입니다',
+    example: 'qweqweqeqwqwqqw',
+  })
   @IsString()
   readonly TokenId: string;
 
+  @ApiProperty({
+    title:'회원가입시 사용자의  id',
+    description: '회원가입시 사용자의 id 입니다',
+    example: 'chanwoo123',
+  })
   @IsString()
   id: string;
 }
 
 export class loginDto {
+  @ApiProperty({
+    title:'로그인시 사용자 토큰',
+    description: '사용자가 로그인시 나오는 토큰입니다.',
+    example: 'qqweqweqwewqeqwewqe',
+  })
   @IsString()
   readonly TokenId: string;
 }
 
 export class userEditDto {
+
+  @ApiProperty({
+    title:'유저 전공',
+    description: '사용자의 전공입니다',
+    example: 'FrontEnd',
+  })
   @IsOptional()
   @IsString({ each: true })
   major: string[];
 
+  @ApiProperty({
+    title:'유저 자기소개',
+    description: '사용자의 간단한 자기소개 입니다',
+    example: '안녕하세요 FrontEnd를 전공하고있는 변찬우 입니다.',
+  })
   @IsOptional()
   @IsString()
   introduce: string;
--- src/portfolio/portfolio.controller.ts
+++ src/portfolio/portfolio.controller.ts
@@ -6,20 +6,31 @@ import {
   Param,
   Patch,
   Post,
+  Req,
 } from '@nestjs/common';
 import { PortfolioService } from './portfolio.service';
 import { PortfolioDTO, PortfolioUpdateDTO } from '../dto/Portfolio.dto';
 import { User } from 'src/entities/user.entity';
+import { Request } from 'express';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('portfolio')
 export class PortfolioController {
   constructor(private readonly portfolioService: PortfolioService) {}
 
+  @ApiOperation({
+    summary: '포트폴리오 가져오기',
+    description: '모든 포트폴리오 가져오기',
+  })
   @Get()
   async getAllPortfolio() {
     return this.portfolioService.getAllPortfolio();
   }
 
+  @ApiOperation({
+    summary: '포트폴리오 생성하기',
+    description: '새로운 포트폴리오를 생성합니다',
+  })
   @Post()
   async postPortfolio(@Body() portfolio: PortfolioDTO) {
     await this.portfolioService.pushData(portfolio);
@@ -29,6 +40,10 @@ export class PortfolioController {
     };
   }
 
+  @ApiOperation({
+    summary: '포트폴리오 수정하기',
+    description: '포트폴리오를 수정합니다',
+  })
   @Patch('/:user/:postName')
   async editPortfolio(
     @Param('user') user: User,
@@ -37,8 +52,27 @@ export class PortfolioController {
   ) {
     this.portfolioService.updatePortfolio(user, postName, UpdateData);
   }
+
+  @ApiOperation({
+    summary: '포트폴리오 삭제하기',
+    description: '포트폴리오를 삭제합니다',
+  })
   @Delete('/:user/:postName')
   async deletePortfolio(
+    @Param('user') user: any,
+    @Param('postName') postName: string,
+    @Req() req: Request,
+  ) {
+    const token = req.cookies['access_token'];
+    const result = await this.portfolioService.right(token, user, postName);
+    //this.portfolioService.deletePortfolio(user, postName);
+    console.log('data', result);
+    if (true) return { message: 'done', status: 200 };
+    else return { message: 'failed', status: 400 };
+  }
+
+  @Get('/:user/:postName')
+  async findPortfolio(
     @Param('user') user: User,
     @Param('postName') postName: string,
   ) {
