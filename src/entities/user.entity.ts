6d19178c13ef4aed4444d46af463ca881a4fe2e6
김현승 <80810303+khs3994@users.noreply.github.com>
2021-12-31 11:15:02 +0900

    swagger 작성 및 userentity 수정
    swagger 작성 및 userentity를 수정 했습니다

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Entity, Column, PrimaryColumn, OneToMany } from 'typeorm';
 import { Portfolio } from './portfolio.entity';
 
@@ -5,29 +6,35 @@ import { Portfolio } from './portfolio.entity';
 export class User {
   
   @PrimaryColumn()
+
+  @ApiProperty({description:'유저 아이디'})
   id: string;
   
+  @ApiProperty({description:'구글 로그인시 유저별 키'})
   @Column()
   sub: string;
 
+  @ApiProperty({description:'유저 이메일'})
   @Column()
   email: string;
 
+  @ApiProperty({description:'유저 이름'})
   @Column()
   name: string;
 
+  @ApiProperty({description:'유저 프로필'})
   @Column()
   picture: string;
 
+  @ApiProperty({description:'유저 전공'})
   @Column('simple-array')
   major: string[];
 
+  @ApiProperty({description:'유저 자기소개'})
   @Column()
   introduce: string;
 
-  @Column()
-  classNum: number;
-
+  @ApiProperty({description:'포트폴리오 테이블'})
   @OneToMany(() => Portfolio, (portfolio) => portfolio.user)
   portfolio: Portfolio[];
 }
